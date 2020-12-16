2a1421cae1a1c9d0bbc2c4a61fdefb892ecd02a0
HagenGaryP <hagengaryp@gmail.com>
2020-12-16 15:26:41 -0500

    EJS tags for conditionals and loops

--- index.js
+++ index.js
@@ -13,4 +13,29 @@ app.get('/', (req, res) => {
   res.render('home.ejs'); // doesn't need the .ejs extension, since we specified it in the set() method.
 });
 
+// route for random response
+// note: Create a template for this, random.ejs, inside of views folder.
+app.get('/rand', (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  // res.render('random');
+  // the render method takes a 2nd argument which is an object of key-value pairs.
+  res.render('random', { rand: num });
+  // now the value of 'num' will be available in the template under the key named "rand".
+})
+
+
+// subreddit route and template
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.render('subreddit', { subreddit });
+})
+
+
+// loops example, using /cats route
+app.get('/cats', (req, res) => {
+  const cats = ['Blue', 'Rocket', 'Monty', 'Winston'];
+
+  res.render('cats', { cats });
+})
+
 app.listen(PORT, () => console.log('listening on port ' + PORT));
